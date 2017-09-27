98338eca8dbb698c2e18df9951843f4591c78c32
jamesrhee91 <jamesshrhee@gmail.com>
2017-09-26 21:34:48 -0400

    mvp done

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -3,19 +3,19 @@ class Api::V1::LocationsController < ApplicationController
   def index
     lat = params[:coords].split('&')[0].gsub('_', '.').to_f
     lon = params[:coords].split('&')[1].gsub('_', '.').to_f
-    nearest = Location.where("ST_Distance(lonlat, 'POINT(#{lon} #{lat})') < 100")
+    nearest = Location.where("ST_Distance(lonlat, 'POINT(#{lon} #{lat})') < 200")
     if nearest.blank?
-      render json: {error: "No parking spaces available"}
+      render json: {empty: []}
     else
       nearest = nearest.map {|e| {lat: e.lonlat.latitude, lng: e.lonlat.longitude}}
-      render json: {coords: nearest}
+      render json: {locations: nearest}
     end
   end
 
   def create
     lon = location_params[:lon]
     lat = location_params[:lat]
-    loc = Location.new(lonlat: "POINT(#{lon} #{lat})")
+    loc = Location.find_or_initialize_by(lonlat: "POINT(#{lon} #{lat})")
     if loc.save
       render json: {success: loc}
     else
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Location.destroy_all
+
+Location.create([
+  {lonlat: "POINT(-74.0147 40.7053)"},
+  {lonlat: "POINT(-74.0120 40.7050)"},
+  {lonlat: "POINT(-74.0140 40.7036)"},
+  {lonlat: "POINT(-74.0148 40.7050)"},
+  {lonlat: "POINT(-74.0139 40.7064)"},
+  {lonlat: "POINT(-74.0152 40.7061)"},
+  {lonlat: "POINT(-74.0129 40.7050)"},
+  {lonlat: "POINT(-74.0125 40.7066)"},
+  {lonlat: "POINT(-74.0121 40.7072)"},
+  {lonlat: "POINT(-74.0140 40.7031)"},
+  {lonlat: "POINT(-74.0127 40.7023)"},
+  {lonlat: "POINT(-74.0130 40.7028)"}
+])
+
+p "Created #{Location.count} locations"
