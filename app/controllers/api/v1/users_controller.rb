ad79327b8b682843d48b4b58b84e323696522e56
jamesrhee91 <jamesshrhee@gmail.com>
2017-10-04 21:44:14 -0400

    pre-demo

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < ApplicationController
 
   def index
     user = User.find(params[:id])
-    locs = user.locations.map {|e| {id: e.id, lat: e.lonlat.latitude, lng: e.lonlat.longitude, time: e.created_at.strftime("%A, %d %b %Y - %l:%M %p")}}
+    locs = user.locations.reverse.map {|e| {id: e.id, lat: e.lonlat.latitude, lng: e.lonlat.longitude, time: e.created_at.strftime("%A, %d %b %Y - %l:%M %p")}}
     render json: {locations: locs}
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,7 @@ Reservation.destroy_all
 
 User.create({first_name: "James", last_name: "Rhee", email: "james.rhee@flatironschool.com", password: "1"})
 
+# Flatiron School area
 Location.create([
   {lonlat: "POINT(-74.0147 40.7053)"},
   {lonlat: "POINT(-74.0120 40.7050)"},
@@ -25,6 +26,35 @@ Location.create([
   {lonlat: "POINT(-74.0127 40.7023)"},
   {lonlat: "POINT(-74.0130 40.7028)"}
 ])
+
+# Times Square area
+Location.create([
+  {lonlat: "POINT(-73.9885 40.7600)"},
+  {lonlat: "POINT(-73.9866 40.7576)"},
+  {lonlat: "POINT(-73.9862 40.7583)"},
+  {lonlat: "POINT(-73.9831 40.7585)"},
+  {lonlat: "POINT(-73.9834 40.7603)"},
+  {lonlat: "POINT(-73.9862 40.7615)"},
+  {lonlat: "POINT(-73.9848 40.7626)"},
+  {lonlat: "POINT(-73.9847 40.7576)"},
+  {lonlat: "POINT(-73.9846 40.7600)"},
+  {lonlat: "POINT(-73.9857 40.7605)"},
+  {lonlat: "POINT(-73.9854 40.7571)"},
+  {lonlat: "POINT(-73.9848 40.7560)"}
+])
+
+# Astoria area
+Location.create([
+  {lonlat: "POINT(-73.9265 40.7634)"},
+  {lonlat: "POINT(-73.9273 40.7645)"},
+  {lonlat: "POINT(-73.9250 40.7653)"},
+  {lonlat: "POINT(-73.9251 40.7637)"},
+  {lonlat: "POINT(-73.9256 40.7631)"},
+  {lonlat: "POINT(-73.9247 40.7649)"},
+  {lonlat: "POINT(-73.9282 40.7638)"} 
+])
+
+
 
 Reservation.create(user_id: 1, location_id: 1)
 Reservation.create(user_id: 1, location_id: 2)
